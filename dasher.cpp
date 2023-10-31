8b742fa61106267f63543c718f5b7b08fe99eea1
Erik <146882681+trangan13@users.noreply.github.com>
2023-10-31 21:45:42 +0000

    Update animation with reset time.

--- dasher.cpp
+++ dasher.cpp
@@ -23,6 +23,12 @@ int main()
     Vector2 scarfyPos;
     scarfyPos.x = WindowWidth/2 - scarfyRec.width/2;
     scarfyPos.y = WindowHeight - scarfyRec.height;
+
+    // frame animation
+    int frame {0};
+    // Amount of time before we update the animation frame
+    const float updateTime{1.0/12.0};
+    float runningTime {};
     
     // Set Velocity
     int velocity {0}; //pixels/frame
@@ -37,50 +43,68 @@ int main()
     SetTargetFPS(60);
 
 
-    // While loop
-    while(!WindowShouldClose()) 
-    {
+        // While loop - Game logic starts
+        while(!WindowShouldClose()) 
+        {
 
-    // Delta time
-    float dt{GetFrameTime()};
-   
-    // Begin Drawing
-    BeginDrawing();
-    ClearBackground(WHITE);
+        // Delta time
+        float dt{GetFrameTime()};
 
-
-   
-    // Ground check
-    if (scarfyPos.y >= WindowHeight - scarfyRec.height)
-    {
-    // Rectangle is on the ground
-    velocity = 0;
-    isInAir = false;
-    }
-    else
-    {
-    // Rectangle is in the air - Add gravity
-    velocity += gravity * dt;
-    isInAir = true;
-    }
-
+             
     
-    // Button Actions - Jump
-    if (IsKeyPressed(KEY_SPACE) && !isInAir)
-    {
-        velocity += jumpVel;
-    }
-
-    // update Y position
-    scarfyPos.y += velocity * dt;
-
-    DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
-
-    // Stop Drawing
-    EndDrawing();
+        // Begin Drawing
+        BeginDrawing();
+        ClearBackground(WHITE);
     
-    
-    }
+        // Ground check
+        if (scarfyPos.y >= WindowHeight - scarfyRec.height)
+        {
+            // Rectangle is on the ground
+            velocity = 0;
+            isInAir = false;
+        }
+        else
+        {
+            // Rectangle is in the air - Add gravity
+            velocity += gravity * dt;
+            isInAir = true;
+        }
+
+        
+        // Button Actions - Jump
+        if (IsKeyPressed(KEY_SPACE) && !isInAir)
+        {
+            velocity += jumpVel;
+        }
+
+        // update Y position
+        scarfyPos.y += velocity * dt;
+
+        // Update Runnning Time - This works as a time, everytime running time reaches 12th of a second, 
+        // we update the frame but also restart the time. 
+        runningTime += dt;
+        if (runningTime >= updateTime)
+        {
+            runningTime = 0.0;
+              // Update Animation Rec
+            scarfyRec.x = frame*scarfyRec.width;
+            frame++; 
+            if (frame > 5)
+            {
+                frame = 0;
+            }
+            
+            
+        }
+        
+
+        DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
+
+        // Stop Drawing
+        EndDrawing();
+        
+        
+        }
     UnloadTexture(scarfy);
     CloseWindow();
 
