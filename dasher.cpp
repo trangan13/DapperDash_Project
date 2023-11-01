888b77a5f22f252ccdd34283b8072882b1c60272
Erik <146882681+trangan13@users.noreply.github.com>
2023-11-01 20:44:38 +0000

    Stopped Animation when jumping / added nebula

--- dasher.cpp
+++ dasher.cpp
@@ -13,7 +13,28 @@ int main()
     // Gravity
     const int gravity {1'000}; // (pixels/s)/s
 
-    // Texture function - Scarfy
+
+    // Nebula variables
+    Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
+    Rectangle nebRec {0.0, 0.0, nebula.width/8, nebula.height/8};
+    // For Rectangle elementars are {x(float), y(float), widht, height}
+    /** This is no longer needed, I we initialized Rectangle earlier. 
+    nebulaRec.width = nebula.width/8; //the size of the actual rectangloe
+    nebulaRec.height = nebula.height/8;
+    nebulaRec.x = 0.0; // where in the sprite we get the image from
+    nebulaRec.y = 0.0;
+    **/
+    
+    Vector2 nebPos {WindowWidth, WindowHeight - nebRec.height};
+    // Again not needed as we initialized directly. 
+    // nebulaPos.x = WindowWidth;
+    //nebulaPos.y = WindowHeight - nebulaRec.height;
+    
+    // Nebula x velocity (pixels/second)
+    int nebVel{-600}; //
+    
+
+    // Scarfy variables
     Texture2D scarfy = LoadTexture("textures/scarfy.png");
     Rectangle scarfyRec;
     scarfyRec.width = scarfy.width/6;
@@ -77,35 +98,44 @@ int main()
             velocity += jumpVel;
         }
 
-        // update Y position
+        // update nebula position
+        nebPos.x += nebVel * dt;
+        // update scarfy position
         scarfyPos.y += velocity * dt;
 
-        // Update Runnning Time - This works as a time, everytime running time reaches 12th of a second, 
+        // Update Runnning Time - This works as a timer, everytime running time reaches 12th of a second, 
         // we update the frame but also restart the time. 
+       if(!isInAir)
+       { 
         runningTime += dt;
         if (runningTime >= updateTime)
         {
             runningTime = 0.0;
-              // Update Animation Rec
+            // Update Animation Rec
             scarfyRec.x = frame*scarfyRec.width;
             frame++; 
             if (frame > 5)
             {
                 frame = 0;
             }
-            
-            
         }
+       }
+       
+      
         
-
+        // Draw Scarfy
         DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
+        // Draw Nebula
+        DrawTextureRec(nebula, nebRec, nebPos, WHITE);
 
         // Stop Drawing
         EndDrawing();
         
         
         }
+   
     UnloadTexture(scarfy);
+    UnloadTexture(nebula);
     CloseWindow();
 
 }
